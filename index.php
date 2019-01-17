ec0d164c514b72936e63a8eb5561b8022370369e
Ramon Barros <ramonfilipe@gmail.com>
2019-01-17 11:38:27 -0300

    criado a classe usuario

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
 require_once("config.php");
-
+/*retorna os usuarios da tabela no banco de dados 
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+$root = new Usuario();
+
+$root->loadByID(3);
+
+echo $root;
 
 ?>
